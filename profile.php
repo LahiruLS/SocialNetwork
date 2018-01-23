35cdd4a60f0d9eb5fec8bc9d709c9971fa5bd476
UchihaLahiru <nine.tail.jinchuriki@gmail.com>
2018-01-23 12:10:15 +0530

    Profile is under construction

--- profile.php
+++ profile.php
@@ -16,21 +16,18 @@ $verified = False;
 if (isset($_GET['username'])){
     if (DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$_GET['username']))){
 
-        $username = DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$_GET['username']))[0]['username'];
+        $username = DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$_GET['username']))[0][username];
         $followerid = Login::isLoggedIn();
-        $userid = DB::query('SELECT id from users WHERE userame=:username',array(':username'=>$username))[0][id];
+        $userid = DB::query('SELECT id from users WHERE userame=:username', array(':username'=>$username))[0][id];
         $verified = DB::query('SELECT verified FROM users WHERE username=:username', array(':username'=>$username))[0][verified];
-
 
         if (isset($_POST['follow'])){
             if ($userid != $followerid) {
                 if (!DB::query('SELECT folower_id FROM followers WHERE user_id=:userid', array(':userid' => $userid))) {
-                    if ($folowerid == 6){
-                        DB::query('UPDATE users SET verified=1 WHERE id=:userid', array(':userid'=>$userid));
-                    }
+                    /*if($followerid == 0){
+                        DB::query('UPDATE users SET verified=1 WHERE id=:userid', array(':userid' => $userid));
+                    }*/
                     DB::query('INSERT INTO folowers (user_id, follower_id) VALUES (:userid, :folowerid) ', array(':userid' => $userid, ':folowerid' => $folowerid));
-                } else {
-                    echo 'Already Following';
                 }
 
                 $isFollowing = True;
@@ -40,9 +37,9 @@ if (isset($_GET['username'])){
         if (isset($_POST['unfollow'])){
             if ($userid != $followerid) {
                 if (DB::query('SELECT folower_id FROM followers WHERE user_id=:userid', array(':userid' => $userid))) {
-                    if ($folowerid == 6){
-                        DB::query('UPDATE users SET verified=0 WHERE id=:userid', array(':userid'=>$userid));
-                    }
+                    /*if($followerid == 0){
+                        DB::query('UPDATE users SET verified=0 WHERE id=:userid', array(':userid' => $userid));
+                    }*/
                     DB::query('DELETE FROM followers WHERE user_id=:userid AND follower_id=:followerid', array(':userid' => $userid, ':folowerid' => $folowerid));
                 }
 
@@ -54,25 +51,41 @@ if (isset($_GET['username'])){
             $isFollowing = True;
         }
 
+        if (isset($_POST['post'])){
+            $postbody = $_POST['postbody'];
+            $userid  = Login::isLoggedIn();
+            DB::query('INSERT INTO posts (postbody, posted_at, user_id, likes) VALUES (:postbody, NOW(), :userid, :folowerid) ', array(':userid' => $userid, ':folowerid' => $folowerid));
+        }
+
 
 
     }else{
-        die("User not Foound");
+        die("User not Found");
     }
 }
 
 ?>
 
-<h1><?php echo $username?>'s Profile'<?php if($verified) {echo ' - Verified';} ?></h1>
+
 <form action="profile.php?username=<?php echo $username; ?>" method="post">
+    <h1><?php echo $username?>'s Profile<?php if($verified) {echo ' - Verified';} ?></h1>
     <?php
         if ($userid != $followerid){
             if($isFollowing){
                 echo '<input type="submit" name="unfollow" value="UnFollow">';
 
-            }else{
+            }else if($userid == $followerid){
+                echo "You can not follow your own";
+            }
+            else{
                 echo '<input type="submit" name="follow" value="Follow">';
             }
         }
     ?>
+</form>
+
+<form action="profile.php?username=<?php echo $username; ?>" method="post">
+    <!textarea name="posttopic" rows="1" cols="80"><!/textarea>
+    <textarea name="postbody" rows="10" cols="80"></textarea>
+    <input type="submit" name="post" value="Post">
 </form>
